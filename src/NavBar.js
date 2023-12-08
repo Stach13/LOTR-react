89324c04cbc7903ac9fae7e71346298766b46dc2
Stach13 <112652730+Stach13@users.noreply.github.com>
2023-12-07 19:32:11 -0500

    adding NavLinks

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink as NavLink } from "react-router-dom";
 
 export const NavBar = () => {
     return (
         <>
             <nav>
                 <ul>
-                    <li><a href="Home.js"><span className="button"><Link to="/">Home</Link></span></a></li>
-                    <li><a href="network.js"><span className="button"><Link to='/network'>Network</Link></span></a></li>
-                    <li><a href="Analysis.html"><span className="button"><Link to='/Analysis'>Analysis</Link></span></a></li>
-                    <li><a href="method.js"><span className="button"><Link to='/method'>Methods</Link></span></a></li>
+                    <li><a href="Home.js"><span className="button"><NavLink to="/">Home</NavLink></span></a></li>
+                    <li><a href="network.js"><span className="button"><NavLink to='/network'>Network</NavLink></span></a></li>
+                    <li><a href="Analysis.html"><span className="button"><NavLink to='/Analysis'>Analysis</NavLink></span></a></li>
+                    <li><a href="method.js"><span className="button"><NavLink to='/method'>Methods</NavLink></span></a></li>
                 </ul>
                 <div className="quotes">
                     <p id="line1"></p>
